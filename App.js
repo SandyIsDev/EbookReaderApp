20890965001e4425e84f51ea80c94b3d66a8b84d
Sandesh Kasliwal <sandeshk@consultbop.com>
2020-12-20 23:39:33 +0530

    Initial Project Setup

---
+++ App.js
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import 'react-native-gesture-handler';
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = ( ) => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Welcome to Ebook Reader App</Text>
+    </View>
+  );
+};
+
+
+export default App;
